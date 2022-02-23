c4c7b84ca0947b17f1a8349581c94d949d1da35d
BHAVITHA17209 <bhavitha17199@gmail.com>
2022-02-24 00:30:55 +0530

    JDBC crud

---
+++ Phase2/Session-3/SimpleDBConnection/src/main/java/com/jdbc/DemoJDBC.java
@@ -0,0 +1,66 @@
+package com.jdbc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoJDBC
+ */
+@WebServlet("/jdbc")
+public class DemoJDBC extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DemoJDBC() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out=response.getWriter();
+		String driver="com.mysql.cj.jdbc.Driver";
+		String url="jdbc:mysql://localhost:3306/simplilearn";
+		String username="root";
+		String password="Bhavitha@17";
+		
+		try {
+			//load driver
+			Class.forName(driver);
+			//get connection and store it in con object
+			Connection con= DriverManager.getConnection(url,username,password);
+			//check if the connection is not null
+			if(con!=null)
+				out.println("Connection Established");
+			else
+				out.println("Error while connecting with database");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
